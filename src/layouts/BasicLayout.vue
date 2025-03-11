9f269eac9b65affdfee54f7f184688b7d0fe6d5c
mac <1753041078@qq.com>
2025-03-11 13:16:31 +0800

    前端初始化（未完成）

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <RouterView />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://codefather.cn" target="_blank">编程导航 by RXXZWW</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+#basicLayout .content {
+  padding: 20px;
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+}
+#basicLayout .header {
+  background: white;
+  color: unset;
+  padding-inline: 20px;
+  margin-bottom: 16px;
+}
+</style>
