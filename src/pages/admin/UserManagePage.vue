7db0e0bb1e53cbc61b4d3a40a623f75a49f63146
mac <1753041078@qq.com>
2025-03-25 11:02:05 +0800

    补充用户管理页面（未完成）

--- src/pages/admin/UserManagePage.vue
+++ src/pages/admin/UserManagePage.vue
@@ -1,9 +1,136 @@
 <template>
-  <h2>用户管理----暂时未开发</h2>
+  <a-form layout="inline" :model="searchParams" @finish="doSearch">
+    <a-form-item label="账号">
+      <a-input v-model:value="searchParams.userAccount" placeholder="输入账号" />
+    </a-form-item>
+    <a-form-item label="用户名">
+      <a-input v-model:value="searchParams.userName" placeholder="输入用户名" />
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" html-type="submit">搜索</a-button>
+    </a-form-item>
+  </a-form>
+  <a-table
+    :columns="columns"
+    :data-source="dataList"
+    :pagination="pagination"
+    @change="doTableChange"
+  >
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'userAvatar'">
+        <a-image :src="record.userAvatar" :width="120" />
+      </template>
+      <template v-else-if="column.dataIndex === 'userRole'">
+        <div v-if="record.userRole === 'admin'">
+          <a-tag color="green">管理员</a-tag>
+        </div>
+        <div v-else>
+          <a-tag color="blue">普通用户</a-tag>
+        </div>
+      </template>
+      <template v-else-if="column.dataIndex === 'createTime'">
+        {{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}
+      </template>
+      <template v-else-if="column.key === 'action'">
+        <a-button type="primary" @click="handleEdit(record)" style="margin-right: 8px"
+          >编辑</a-button
+        >
+        <a-button type="primary" danger @click="handleDelete(record)">删除</a-button>
+      </template>
+    </template>
+  </a-table>
 </template>
+<script lang="ts" setup>
+import { SmileOutlined } from '@ant-design/icons-vue'
+import { ref } from 'vue'
+import { listUserVoByPageUsingPost } from '@/api/basicController'
+import { message } from 'ant-design-vue'
+import { onMounted, reactive } from 'vue'
+import dayjs from 'dayjs'
+import { computed } from 'vue'
 
-<script setup lang='ts'>
+const columns = [
+  {
+    name: 'id',
+    title: 'id',
+    dataIndex: 'id',
+  },
+  {
+    name: '账号',
+    title: '账号',
+    dataIndex: 'userAccount',
+  },
+  {
+    title: '用户名',
+    dataIndex: 'userName',
+  },
+  {
+    title: '头像',
+    dataIndex: 'userAvatar',
+  },
+  {
+    title: '简介',
+    dataIndex: 'userProfile',
+  },
+  {
+    title: '用户角色',
+    dataIndex: 'userRole',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+  },
+  {
+    title: '操作',
+    key: 'action',
+  },
+]
+
+//数据
+const dataList = ref([])
+const total = ref(0)
+
+//搜索条件
+const searchParams = reactive<API.UserQueryRequest>({
+  current: 1,
+  pageSize: 10,
+})
+
+//获取数据
+const fetchData = async () => {
+  const res = await listUserVoByPageUsingPost({
+    ...searchParams,
+  })
+  if (res.data.data) {
+    //@ts-expect-error 这里是为了方便，实际应该是res.data.data.records
+    dataList.value = res.data.data.records ?? []
+    total.value = res.data.data.total ?? 0
+  } else {
+    message.error('获取数据失败' + res.data.message)
+  }
+}
+// 页面加载时请求一次
+onMounted(() => {
+  fetchData()
+})
+// 分页参数
+const pagination = computed(() => {
+  return {
+    current: searchParams.current ?? 1,
+    pageSize: searchParams.pageSize ?? 10,
+    total: total.value,
+    showSizeChanger: true,
+    showTotal: (total) => `共 ${total} 条`,
+  }
+})
+const doTableChange = () => {}
+
+const doSearch = () => {}
+
+const handleEdit = (record: API.UserVO) => {
+  console.log('编辑', record)
+}
+const handleDelete = (record: API.UserVO) => {}
 </script>
 
-<style>
-</style>
+<style scoped></style>
